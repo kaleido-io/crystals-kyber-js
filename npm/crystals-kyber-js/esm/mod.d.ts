e7ce1a02c31847b04213b367bc88af11e808f7e8
Matthew Gregoire <matthew.gregoire@kaleido.io>
2025-06-04 16:52:04 -0400

    Rebuilt to export the newly exposed functions
    Signed-off-by: Matthew Gregoire <matthew.gregoire@kaleido.io>

--- npm/crystals-kyber-js/esm/mod.d.ts
+++ npm/crystals-kyber-js/esm/mod.d.ts
@@ -2,4 +2,5 @@ export { MlKemError } from "./src/errors.js";
 export { MlKem512 } from "./src/mlKem512.js";
 export { MlKem768 } from "./src/mlKem768.js";
 export { MlKem1024 } from "./src/mlKem1024.js";
+export { polyFromBytes } from "./src/mlKemBase.js";
 //# sourceMappingURL=mod.d.ts.map
